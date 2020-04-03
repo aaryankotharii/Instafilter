a0180ac7836e27e33e36eaab7a3b57f7743c7a15
aaryankotharii <aaryan.kothari@gmail.com>
2020-04-03 15:17:43 +0530

    adding filters to images

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -5,47 +5,89 @@
 //  Created by Aaryan Kothari on 03/04/20.
 //  Copyright © 2020 Aaryan Kothari. All rights reserved.
 //
-
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
-struct ContentView2: View {
-    @State private var blurAmouunt : CGFloat = 0 {
-        didSet{
-            print(blurAmouunt)
-        }
+
+struct ContentView : View{
+    @State private var image : Image?
+  
+    var  body : some View {
+        VStack{
+            image?
+            .resizable()
+            .scaledToFit()
+            }.onAppear(perform: loadImage)
     }
-    var body: some View {
-        VStack {
-            Text("Hello, World!")
-                .blur(radius: blurAmouunt)
-            Slider(value: $blurAmouunt, in: 0...20)
+    func loadImage(){
+        guard let imputImage = UIImage(named: "Example") else {
+            return
         }
+        
+        let beginImage = CIImage(image: imputImage)
+        
+        //more code to come
+        
+        let context = CIContext()
+        
+        let  currentFilter = CIFilter.sepiaTone()
+        
+        currentFilter.inputImage = beginImage
+        currentFilter.intensity = 0.5
+        
+        
+        guard let outputImage = currentFilter.outputImage else {return}
+        
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent){
+            let uiImage = UIImage(cgImage: cgimg)
+            
+            image = Image(uiImage: uiImage)
+        }
+        
     }
 }
 
-struct ContentView : View{
-    @State private var showingActionSheet = false
-    @State private var backgroundColor = Color.white
-    var body : some View {
-        Text("Hello, world!")
-        .frame(width: 300, height: 300)
-        .background(backgroundColor)
-            .onTapGesture {
-                self.showingActionSheet = true
-        }.actionSheet(isPresented: $showingActionSheet) {
-            ActionSheet(title: Text("Change Background"), message: Text("Select new colour"), buttons: [
-                .default(Text("Red")){ self.backgroundColor = .red },
-                .default(Text("Green")){ self.backgroundColor = .green },
-                .default(Text("Blue")){ self.backgroundColor = .blue },
-                .default(Text("yelllow")){ self.backgroundColor = .yellow },
-                .cancel()
-            ])
-        }
-    }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+//struct ContentView2: View {
+//    @State private var blurAmouunt : CGFloat = 0 {
+//        didSet{
+//            print(blurAmouunt)
+//        }
+//    }
+//    var body: some View {
+//        VStack {
+//            Text("Hello, World!")
+//                .blur(radius: blurAmouunt)
+//            Slider(value: $blurAmouunt, in: 0...20)
+//        }
+//    }
+//}
+//
+//struct ContentView : View{
+//    @State private var showingActionSheet = false
+//    @State private var backgroundColor = Color.white
+//    var body : some View {
+//        Text("Hello, world!")
+//        .frame(width: 300, height: 300)
+//        .background(backgroundColor)
+//            .onTapGesture {
+//                self.showingActionSheet = true
+//        }.actionSheet(isPresented: $showingActionSheet) {
+//            ActionSheet(title: Text("Change Background"), message: Text("Select new colour"), buttons: [
+//                .default(Text("Red")){ self.backgroundColor = .red },
+//                .default(Text("Green")){ self.backgroundColor = .green },
+//                .default(Text("Blue")){ self.backgroundColor = .blue },
+//                .default(Text("yelllow")){ self.backgroundColor = .yellow },
+//                .cancel()
+//            ])
+//        }
+//    }
+//}
+//
