191d0487a66285fc6603e91091d7cd2087b9290b
aaryankotharii <aaryan.kothari@gmail.com>
2020-04-03 15:01:34 +0530

    actiomSheet implemented

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -8,9 +8,39 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ContentView2: View {
+    @State private var blurAmouunt : CGFloat = 0 {
+        didSet{
+            print(blurAmouunt)
+        }
+    }
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Hello, World!")
+                .blur(radius: blurAmouunt)
+            Slider(value: $blurAmouunt, in: 0...20)
+        }
+    }
+}
+
+struct ContentView : View{
+    @State private var showingActionSheet = false
+    @State private var backgroundColor = Color.white
+    var body : some View {
+        Text("Hello, world!")
+        .frame(width: 300, height: 300)
+        .background(backgroundColor)
+            .onTapGesture {
+                self.showingActionSheet = true
+        }.actionSheet(isPresented: $showingActionSheet) {
+            ActionSheet(title: Text("Change Background"), message: Text("Select new colour"), buttons: [
+                .default(Text("Red")){ self.backgroundColor = .red },
+                .default(Text("Green")){ self.backgroundColor = .green },
+                .default(Text("Blue")){ self.backgroundColor = .blue },
+                .default(Text("yelllow")){ self.backgroundColor = .yellow },
+                .cancel()
+            ])
+        }
     }
 }
 
